c5137f7d6cbcca413f782c737347163dde5e456f
jinwoo <rudwlsdn@naver.com>
2024-02-26 19:21:06 +0900

    init

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,129 +1,80 @@
 <script>
-	import { page } from '$app/stores';
-	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
+    import { LightSwitch } from "@skeletonlabs/skeleton";
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
-	</div>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
-			<img src={github} alt="GitHub" />
-		</a>
-	</div>
-</header>
 
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
+    #head-container {
+        width: 100%;
+        height: 70px;
+        box-shadow: 0 0 100px rgba(0, 0, 0, 0.3);
+    }
+    
+    #limit-box {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        max-width: 1280px;
+        height: 100%;
+        margin: 0 auto;
+    }
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
+    #logo-box {
+        order: 1;
+        height: 100%;
+    }
+    
+    #logo-box > a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+        font-size: 28px;
+    }
 
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
+    #menu-box,
+    #light-box {
+        width: 60px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    
+    #menu-box {
+        order: 0;
+    }
 
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
+    #menu-box button {
+        width: 50px;
+        height: 50px;
+    }
+    
+    #light-box {
+        order: 2;
+    }
 </style>
+
+
+
+
+<header id="head-container" class="bg-surface-100-800-token">
+    <div id="limit-box">
+        <h1 id="logo-box">
+            <a href="/">JinWoo's Note</a>
+        </h1>
+    
+        <div id="menu-box">
+            <button type="button" class="btn-icon"></button>
+        </div>
+    
+        <div id="light-box">
+            <LightSwitch />
+        </div>
+    </div>
+</header>
+
+
+
+<!-- <a href="https://www.flaticon.com/kr/free-icons/" title="메뉴 아이콘">메뉴 아이콘  제작자: ariefstudio - Flaticon</a> -->
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
+import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), purgeCss()],
+	resolve: {
+		alias: {
+			"@lib": path.resolve(__dirname, "./src/lib"),
+			"@routes": path.resolve(__dirname, "./src/routes"),
+			"@static": path.resolve(__dirname, "./src/static"),
+		}
+	}
 });
